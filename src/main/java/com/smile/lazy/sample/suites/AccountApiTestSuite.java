b5da3de0e4b425fb9fcb2c4614507511d8eaac9d
hasithag <hasitha.gamage@sysco.com>
2020-03-04 19:31:07 +0530

    working status

--- src/main/java/com/smile/lazy/sample/suites/AccountApiTestSuite.java
+++ src/main/java/com/smile/lazy/sample/suites/AccountApiTestSuite.java
@@ -7,7 +7,7 @@ import static com.smile.lazy.sample.scenarios.CreateAccountTestScenario.getAccou
 public class AccountApiTestSuite {
 
     public static TestSuite getAccountApiTestSuite() {
-        TestSuite testSuite1 = new TestSuite("Smile-Test-Suite-1", "Account Test Suite");
+        TestSuite testSuite1 = new TestSuite("Smile-Account-API-Test-Suite-1", "Account Test Suite");
         testSuite1.getTestScenarios().add(getAccountCreationTestScenario());
         return testSuite1;
     }
